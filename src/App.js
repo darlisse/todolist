5515e681685f16952e1bc9f5aa8d41f79995114d
darlisse <darlissecayra.dumaliang@tup.edu.ph>
2023-12-26 21:12:07 +0800

    modified to create to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [editingIndex, setEditingIndex] = useState(null); // Fixed case sensitivity
+
+  // Function to add a new todo
+  const handleAddTodo = () => {
+    setTodos([...todos, inputText]);
+    setInputText("");
+  };
+
+  // Function to toggle a todo's completion state
+  const handleToggleComplete = (index) => {
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleEditTodo = (index) => {
+    setEditingIndex(index);
+  };
+
+  // Function to delete a todo
+  const handleDeleteTodo = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ToDo">
+      <h1>To Do List</h1>
+      <input
+        type="text"
+        className="text"
+        value={inputText} // Using inputText, not todo
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="What are you into right now?"
+      />
+      <button onClick={handleAddTodo} className="Button">
+        Add
+      </button>
+      <ul className="ToDoList">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {editingIndex === index ? (
+              <input
+                type="text"
+                className="updateText"
+                value={todo}
+                onChange={(e) =>
+                  setTodos(
+                    todos.map((t, i) => (i === index ? e.target.value : t))
+                  )
+                }
+                onBlur={() => setEditingIndex(null)}
+              />
+            ) : (
+              <span>{todo.completed ? <strike>{todo}</strike> : todo}</span>
+            )}
+            {/* Removed duplicate todo rendering
+            <button onClick={() => handleToggleComplete(index)}>
+              {todo.completed ? "Undo" : "Complete"}
+            </button> */}
+            <button onClick={() => handleDeleteTodo(index)} className="Button">
+              Delete
+            </button>
+            <button onClick={() => handleEditTodo(index)} className="Button">
+              Update
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
